c3f657c091ff385a27ae7c12cde4812dbdb57313
Steve Gardner <sjgardne@gmail.com>
2018-02-05 21:31:12 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -3,5 +3,5 @@ var index = require("./index.js");
 if (index.name === "Susan") {
     console.log("The name is correct")}
 
-if (index.height === 70) {
+if (index.height === "70") {
     console.log("The height is correct")}
