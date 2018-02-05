38844378860b6897f3d1bf27e17a0cc12b99cd18
Steve Gardner <sjgardne@gmail.com>
2018-02-05 21:04:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var name = "Susan" ;
+var name = "Joe" ;
 var height = "70" ; 
 
 if (name === "Susan"){console.log("The name is correct")} 
 else { console.log("Expected the name to be Susan")}
 
-if (index.height === 70) {
-    console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
+
 
 
 
